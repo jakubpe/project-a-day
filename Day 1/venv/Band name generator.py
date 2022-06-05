10f55576bb9d1ea24acdc198f1be62968029f3a2
jakubpe <48805061+jakubpe@users.noreply.github.com>
2022-06-05 20:53:21 +0200

    Delete Day 1 directory

--- Day 1/venv/Band name generator.py
+++
@@ -1,5 +0,0 @@
-print("Hi, nice to see you\nWe will generate band name just for you!\nLet's start")
-city = input("Where are you from?\n")
-pet_name = input("What is the name of your pet?\n")
-band_name = city + " " + pet_name
-print("The name just for you is:\n{}".format(band_name))
